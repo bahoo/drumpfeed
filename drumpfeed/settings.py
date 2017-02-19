a3ea0b9d7a5ed57fdf395af523c6cf1cbe7a5c74
bahoo <jon.c.culver@gmail.com>
2017-02-18 17:02:14 -0800

    Cache ruling everything around me.

--- drumpfeed/settings.py
+++ drumpfeed/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'drumpfeed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config()}
+# DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
--- drumpfeed/views.py
+++ drumpfeed/views.py
@@ -1,4 +1,5 @@
 from django.contrib.syndication.views import Feed
+from django.core.cache import cache
 from django.urls import reverse
 from bs4 import BeautifulSoup
 import datetime, requests
@@ -9,9 +10,25 @@ class LatestEntriesFeed(Feed):
     link = "/"
     description = "Tracking Donald's dumb things."
 
+    def __call__(self, request, *args, **kwargs):
+      # Cache the RSS feed
+      # It has to be done this way because the Feed class is a callable, not a view.
+      cache_key = self.get_cache_key(*args, **kwargs)
+      response = cache.get(cache_key)
+
+      if response is None:
+        response = super(LatestEntriesFeed, self).__call__(request, *args, **kwargs)
+        cache.set(cache_key, response, 1800) # cache for 15 minutes
+
+      return response
+
+    def get_cache_key(self, *args, **kwargs):
+      # Override this in subclasses for more caching control
+      return "%s-%s" % (self.__class__.__module__, '/'.join([ "%s,%s" % (key, val) for key,val in kwargs.items() ])) 
+
     def items(self):
         req = requests.get('https://www.whitehouse.gov/blog')
-        soup = BeautifulSoup(req.text)
+        soup = BeautifulSoup(req.text, "html.parser")
         return soup.select('.view-press-office-listings .views-row')
 
     def item_pubdate(self, item):
@@ -23,7 +40,7 @@ class LatestEntriesFeed(Feed):
 
     def item_description(self, item):
         req = requests.get(self.item_link(item))
-        soup = BeautifulSoup(req.text)
+        soup = BeautifulSoup(req.text, "html.parser")
         return soup.select('.field-item p')[0].text.strip()
 
     def item_link(self, item):
